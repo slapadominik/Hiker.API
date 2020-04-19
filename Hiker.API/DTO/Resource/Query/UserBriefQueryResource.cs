9cb548f338864d0bb45389aa327941fc25997346
Dominik Słapa <dominik.slapa@gmail.com>
2020-04-19 10:19:34 +0200

    feat: add Birthday field to UserBriefQueryResource

--- Hiker.API/DTO/Resource/Query/UserBriefQueryResource.cs
+++ Hiker.API/DTO/Resource/Query/UserBriefQueryResource.cs
@@ -7,6 +7,7 @@ namespace Hiker.API.DTO.Resource.Briefs
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
         public string ProfilePictureUrl { get; set; }
     }
 }
